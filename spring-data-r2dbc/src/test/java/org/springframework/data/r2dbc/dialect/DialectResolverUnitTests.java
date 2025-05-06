399e6f0f9b149db44496e64ab80b9f0d7f21be9d
liubao68 <bismy@qq.com>
2025-05-06 21:16:15 +0800

    remove not suitable test case and add extension

--- spring-data-r2dbc/src/test/java/org/springframework/data/r2dbc/dialect/DialectResolverUnitTests.java
+++ spring-data-r2dbc/src/test/java/org/springframework/data/r2dbc/dialect/DialectResolverUnitTests.java
@@ -1,22 +1,11 @@
 package org.springframework.data.r2dbc.dialect;
 
-import static org.assertj.core.api.Assertions.*;
-
-import io.asyncer.r2dbc.mysql.MySqlConnectionConfiguration;
-import io.asyncer.r2dbc.mysql.MySqlConnectionFactory;
-import io.r2dbc.h2.H2ConnectionConfiguration;
-import io.r2dbc.h2.H2ConnectionFactory;
-import io.r2dbc.postgresql.PostgresqlConnectionConfiguration;
-import io.r2dbc.postgresql.PostgresqlConnectionFactory;
-import io.r2dbc.spi.Connection;
-import io.r2dbc.spi.ConnectionFactory;
-import io.r2dbc.spi.ConnectionFactoryMetadata;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
-import org.mariadb.r2dbc.MariadbConnectionConfiguration;
-import org.mariadb.r2dbc.MariadbConnectionFactory;
 import org.reactivestreams.Publisher;
 import org.springframework.data.relational.core.dialect.LimitClause;
 import org.springframework.data.relational.core.dialect.LockClause;
@@ -24,30 +13,16 @@ import org.springframework.data.relational.core.sql.LockOptions;
 import org.springframework.data.relational.core.sql.render.SelectRenderContext;
 import org.springframework.r2dbc.core.binding.BindMarkersFactory;
 
+import io.r2dbc.spi.Connection;
+import io.r2dbc.spi.ConnectionFactory;
+import io.r2dbc.spi.ConnectionFactoryMetadata;
+
 /**
  * Unit tests for {@link DialectResolver}.
  *
  * @author Mark Paluch
  */
 public class DialectResolverUnitTests {
-
-	@Test // GH-20, GH-104, GH-1475
-	void shouldResolveDatabaseType() {
-
-		PostgresqlConnectionFactory postgres = new PostgresqlConnectionFactory(PostgresqlConnectionConfiguration.builder()
-				.host("localhost").database("foo").username("bar").password("password").build());
-		H2ConnectionFactory h2 = new H2ConnectionFactory(H2ConnectionConfiguration.builder().inMemory("mem").build());
-		MariadbConnectionFactory mariadb = new MariadbConnectionFactory(
-				MariadbConnectionConfiguration.builder().socket("/foo").username("bar").build());
-		MySqlConnectionFactory mysql = MySqlConnectionFactory
-				.from(MySqlConnectionConfiguration.builder().host("foo").username("bar").build());
-
-		assertThat(DialectResolver.getDialect(postgres)).isEqualTo(PostgresDialect.INSTANCE);
-		assertThat(DialectResolver.getDialect(h2)).isEqualTo(H2Dialect.INSTANCE);
-		assertThat(DialectResolver.getDialect(mariadb)).isEqualTo(MySqlDialect.INSTANCE);
-		assertThat(DialectResolver.getDialect(mysql)).isEqualTo(MySqlDialect.INSTANCE);
-	}
-
 	@Test // gh-20, gh-104
 	void shouldNotResolveUnknownDatabase() {
 		assertThatThrownBy(() -> DialectResolver.getDialect(new ExternalConnectionFactory("unknown")))
